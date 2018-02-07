537fe4779a64ddf72dcefe99727c34c6d3b3e2a8
Vaishali Bakde <Vaishali Bakde@DESKTOP-V30R0IS>
2018-02-07 14:16:54 +0530

    1-updated the  landing page of website 2-added login button to page for manager login 3-added three buttons for the manager details

--- app/BookMyTable/Controllers/LoginController.cs
+++ app/BookMyTable/Controllers/LoginController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using BookMyTableDataLayer.Data;
 
 namespace BookMyTable.Controllers
 {
+    
     public class LoginController : Controller
     {
+        bookmytableEntities1 db = new bookmytableEntities1();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+       
+        public ActionResult Index(FormCollection collection)
+        {
+            string email = collection.Get("email");
+            string Password = collection.Get("Password");
+            if (email == "admin@gmail.com" && Password == "12345")
+            {
+                return Redirect("~/Manager/index");
+            }
+            else
+            {
+                ViewBag.Message = "Please enter valid Email ID and Password";
+
+            }
+            return RedirectToAction("index");
+        }
     }
 }
+    
